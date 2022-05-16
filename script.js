261b5d09ef433a4872a59430c6c4796b43d3e1ef
dsandelc <diegosanchezd@hotmail.com>
2022-05-16 11:52:17 +0200

    Punto dos realizado

--- script.js
+++ script.js
@@ -20,31 +20,39 @@ window.addEventListener('load', onLoad);
 function onLoad() {
 
     // Selecciono el elemento con la clase seleted
-    let li_3 = document.getElementById('selected');
+    let spam_3 = document.getElementById('selected');
+    console.log(spam_3);
+
+    let spam_3B = document.querySelectorAll("span.selected");
+    console.log(spam_3B);
+
+    // Selecciono el elemento padre
+    let li_3 = spam_3B[0].parentNode;
     console.log(li_3);
 
     // Selecciono el elemento padre
-    let lista = li_3.parentNode;
-    console.log(lista);
-
-    // Selecciono el elemento padre
-    let ul = lista.parentNode;
+    let ul = li_3.parentNode;
     console.log(ul);
 
     // Hijos ul
     let hijosUl = ul.childNodes;
     console.log(hijosUl);
 
-    for (let i = 0; i < hijosUl.length; i++) {
-        let hijosLi = hijosUl[i].childNodes;
-        console.log(hijosLi);
-    }
+    // Hijos li
+    // var span = [];
+    // for (let i = 0; i < hijosUl.length; i++) {
+    //     let hijosLi = hijosUl[i].childNodes;
+    //     span.push = hijosLi;
+    //     console.log(hijosLi);
+    // }
+
+    // console.log(span);
 
 
     // Selecciono todos los elementos span
     let span = document.querySelectorAll('span');
 
-    //Contador de elementos
+    // Contador de elementos
     var cont = 0;
     // Bucle para añadir clases
     for (var i = 0; i < span.length; i++) {
@@ -52,7 +60,29 @@ function onLoad() {
         span[i].className += " element-" + increneto;
     }
 
+    // Selecciono todos los elementos li
     let li = document.querySelectorAll('li');
     console.log(li);
+
+    // Selecciono los elementos pares
+    var arrayPares = [];
+    for (let i = 0; i < li.length; i++) {
+
+        if (i % 2 != 0) {
+            // Compruebo que entra en el if
+            console.log("Entra en if");
+            // Guardo cada iteración que cumple la condición
+            liPares = li[i];
+            console.log(liPares);
+            // Añado cada iteración a array
+            arrayPares.push(liPares);
+            // Elimino loe elementos pares
+            liPares.remove();
+        };
+    }
+    //Muestro el erray de elementos pares
+    console.log(arrayPares);
+
+
 
 }
